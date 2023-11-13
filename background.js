ac2e6184bb9cfcf655f55a6d60b8eb1a3195a77f
Max Lee <maxjerry0107@gmail.com>
2023-11-13 12:31:54 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,84 @@
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.action.setBadgeText({
+    text: 'OFF'
+  });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url?.startsWith("chrome://")) return;
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState
+  });
+
+  if (nextState === 'ON') {
+    await chrome.scripting.insertCSS({
+      files: ['blur.css'],
+      target: { tabId: tab.id }
+    });
+  } else if (nextState === 'OFF') {
+    await chrome.scripting.removeCSS({
+      files: ['blur.css'],
+      target: { tabId: tab.id }
+    });
+  }
+});
+
+chrome.tabs.onActivated.addListener(async (tabInfo) => {
+  const tab = await chrome.tabs.get(tabInfo.tabId);
+  if (!tab.url) return;
+  if (tab.url?.startsWith("https://www.linkedin.com/")) {
+    await chrome.scripting.removeCSS({
+      files: ['linkedin.css'],
+      target: { tabId: tabInfo.tabId }
+    });
+    await chrome.scripting.insertCSS({
+      files: ['linkedin.css'],
+      target: { tabId: tabInfo.tabId }
+    });
+  };
+  await chrome.action.setBadgeText({
+    tabId: tabInfo.tabId,
+    text: "ON"
+  });
+  await chrome.scripting.removeCSS({
+    files: ['blur.css'],
+    target: { tabId: tabInfo.tabId }
+  });
+  await chrome.scripting.insertCSS({
+    files: ['blur.css'],
+    target: { tabId: tabInfo.tabId }
+  });
+})
+
+chrome.webNavigation.onCommitted.addListener(async (details) => {
+  const tab = await chrome.tabs.get(details.tabId);
+  if (!tab.url) return;
+  if (tab.url?.startsWith("https://www.linkedin.com/")) {
+    await chrome.scripting.removeCSS({
+      files: ['linkedin.css'],
+      target: { tabId: details.tabId }
+    });
+    await chrome.scripting.insertCSS({
+      files: ['linkedin.css'],
+      target: { tabId: details.tabId }
+    });
+  };
+  if (details.frameType == "outermost_frame") {
+    await chrome.action.setBadgeText({
+      tabId: details.tabId,
+      text: "ON"
+    });
+    await chrome.scripting.removeCSS({
+      files: ['blur.css'],
+      target: { tabId: details.tabId }
+    });
+    await chrome.scripting.insertCSS({
+      files: ['blur.css'],
+      target: { tabId: details.tabId }
+    });
+  }
+});
