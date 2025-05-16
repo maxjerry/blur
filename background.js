c6ff730a3202f17f91497c7af4c2c8be414f56b0
Max Lee <maxjerry0107@gmail.com>
2025-05-15 21:42:01 -0400

    removed iframe blur

--- background.js
+++ background.js
@@ -1,28 +1,31 @@
 chrome.runtime.onInstalled.addListener(async () => {
   await chrome.action.setBadgeText({
-    text: 'OFF'
+    text: "OFF",
   });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url?.startsWith("chrome://")) return;
+  if (tab.url?.includes("localhost")) return;
+  if (tab.url?.includes("127.0.0.1")) return;
+  if (tab.url?.includes("meet.google.com")) return;
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+  const nextState = prevState === "ON" ? "OFF" : "ON";
 
   await chrome.action.setBadgeText({
     tabId: tab.id,
-    text: nextState
+    text: nextState,
   });
 
-  if (nextState === 'ON') {
+  if (nextState === "ON") {
     await chrome.scripting.insertCSS({
-      files: ['blur.css'],
-      target: { tabId: tab.id }
+      files: ["blur.css"],
+      target: { tabId: tab.id },
     });
-  } else if (nextState === 'OFF') {
+  } else if (nextState === "OFF") {
     await chrome.scripting.removeCSS({
-      files: ['blur.css'],
-      target: { tabId: tab.id }
+      files: ["blur.css"],
+      target: { tabId: tab.id },
     });
   }
 });
@@ -30,55 +33,63 @@ chrome.action.onClicked.addListener(async (tab) => {
 chrome.tabs.onActivated.addListener(async (tabInfo) => {
   const tab = await chrome.tabs.get(tabInfo.tabId);
   if (!tab.url) return;
+  if (tab.url?.includes("localhost")) return;
+  if (tab.url?.includes("127.0.0.1")) return;
+  if (tab.url?.startsWith("chrome://")) return;
+  if (tab.url?.includes("meet.google.com")) return;
   if (tab.url?.startsWith("https://www.linkedin.com/")) {
     await chrome.scripting.removeCSS({
-      files: ['linkedin.css'],
-      target: { tabId: tabInfo.tabId }
+      files: ["linkedin.css"],
+      target: { tabId: tabInfo.tabId },
     });
     await chrome.scripting.insertCSS({
-      files: ['linkedin.css'],
-      target: { tabId: tabInfo.tabId }
+      files: ["linkedin.css"],
+      target: { tabId: tabInfo.tabId },
     });
-  };
+  }
   await chrome.action.setBadgeText({
     tabId: tabInfo.tabId,
-    text: "ON"
+    text: "ON",
   });
   await chrome.scripting.removeCSS({
-    files: ['blur.css'],
-    target: { tabId: tabInfo.tabId }
+    files: ["blur.css"],
+    target: { tabId: tabInfo.tabId },
   });
   await chrome.scripting.insertCSS({
-    files: ['blur.css'],
-    target: { tabId: tabInfo.tabId }
+    files: ["blur.css"],
+    target: { tabId: tabInfo.tabId },
   });
-})
+});
 
 chrome.webNavigation.onCommitted.addListener(async (details) => {
   const tab = await chrome.tabs.get(details.tabId);
+  if (tab.url?.includes("localhost")) return;
+  if (tab.url?.includes("127.0.0.1")) return;
+  if (tab.url?.startsWith("chrome://")) return;
+  if (tab.url?.includes("meet.google.com")) return;
   if (!tab.url) return;
   if (tab.url?.startsWith("https://www.linkedin.com/")) {
     await chrome.scripting.removeCSS({
-      files: ['linkedin.css'],
-      target: { tabId: details.tabId }
+      files: ["linkedin.css"],
+      target: { tabId: details.tabId },
     });
     await chrome.scripting.insertCSS({
-      files: ['linkedin.css'],
-      target: { tabId: details.tabId }
+      files: ["linkedin.css"],
+      target: { tabId: details.tabId },
     });
-  };
+  }
   if (details.frameType == "outermost_frame") {
     await chrome.action.setBadgeText({
       tabId: details.tabId,
-      text: "ON"
+      text: "ON",
     });
     await chrome.scripting.removeCSS({
-      files: ['blur.css'],
-      target: { tabId: details.tabId }
+      files: ["blur.css"],
+      target: { tabId: details.tabId },
     });
     await chrome.scripting.insertCSS({
-      files: ['blur.css'],
-      target: { tabId: details.tabId }
+      files: ["blur.css"],
+      target: { tabId: details.tabId },
     });
   }
 });
